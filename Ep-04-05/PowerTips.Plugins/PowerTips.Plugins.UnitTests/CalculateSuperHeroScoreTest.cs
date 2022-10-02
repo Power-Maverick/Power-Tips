f80b44cddd558f165525105c5184edc0ed66bcf2
Power-Maverick <danish.naglekar@hotmail.com>
2022-10-02 09:11:21 -0400

    Uni Test Project - Ep.6

---
+++ Ep-04-05/PowerTips.Plugins/PowerTips.Plugins.UnitTests/CalculateSuperHeroScoreTest.cs
@@ -0,0 +1,55 @@
+﻿using FakeXrmEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using PowerTips.Plugins.Training;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PowerTips.Plugins.UnitTests
+{
+    [TestClass]
+    public class CalculateSuperHeroScoreTest
+    {
+        [TestMethod]
+        public void ExecutePluginTest()
+        {
+            // Init
+            var context = new XrmFakedContext();
+            var pluginContext = context.GetDefaultPluginContext();
+
+            // Prepare
+            var id = Guid.NewGuid();
+
+            var target = new Entity("pmav_superhero") { Id = id };
+            target.Attributes.Add("powm_showhidepowerstats", true);
+
+            var postImage = new Entity("pmav_superhero") { Id = id };
+            postImage.Attributes.Add("powm_showhidepowerstats", true);
+            postImage.Attributes.Add("pmav_stats_intelligence", 50);
+            postImage.Attributes.Add("pmav_stats_strength", 50);
+            postImage.Attributes.Add("pmav_stats_speed", 50);
+            postImage.Attributes.Add("pmav_stats_durability", 50);
+            postImage.Attributes.Add("pmav_stats_power", 50);
+            postImage.Attributes.Add("pmav_stats_combat", 50);
+
+            ParameterCollection inputParameters = new ParameterCollection();
+            inputParameters.Add("Target", target);
+
+            EntityImageCollection postImages = new EntityImageCollection();
+            postImages.Add("PostImage", postImage);
+
+            pluginContext.InputParameters = inputParameters;
+            pluginContext.PostEntityImages = postImages;
+
+            context.Initialize(new List<Entity>() { postImage });
+
+            // Execute
+            context.ExecutePluginWith<CalculateSuperHeroScore>(pluginContext);
+
+            // Assert
+            var superHeroAfterExecution = context.CreateQuery("pmav_superhero").FirstOrDefault();
+            Assert.AreEqual("50", superHeroAfterExecution["powm_combinedpowerstats"]);
+        }
+    }
+}
---
+++ Ep-04-05/PowerTips.Plugins/PowerTips.Plugins.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PowerTips.Plugins.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PowerTips.Plugins.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("236d6b88-16fd-44cb-9730-358a573303a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
